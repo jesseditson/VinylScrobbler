d8e4ee97ee250c435305d3fd9093a8b097ea9ca4
Jesse Ditson <jesse.ditson@gmail.com>
2017-02-04 15:56:00 -0800

    automatically log in users with credentials

--- VinylScrobbler/AppDelegate.swift
+++ VinylScrobbler/AppDelegate.swift
@@ -16,6 +16,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if LastfmAPI.sharedInstance.authenticated {
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            
+            let initialViewController = storyboard.instantiateViewController(withIdentifier: "RecordingViewController")
+            
+            self.window?.rootViewController = initialViewController
+            self.window?.makeKeyAndVisible()
+        }
+        
         return true
     }
 
