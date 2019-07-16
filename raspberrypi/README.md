088eb41942411f9b9b7925432d07ab92a3bb4e74
Jesse Ditson <jesse.ditson@gmail.com>
2019-07-15 20:08:29 -0700

    Add on-pi strategy

---
+++ raspberrypi/README.md
@@ -0,0 +1,7 @@
+- symlink `daemon.sh` to `/etc/init.d/`:
+
+```
+ln -s /home/pi/checkouts/VinylScrobbler/master/raspberrypi/daemon.sh /etc/init.d/vinyl-scrobbler
+```
+
+- run `sudo update-rc.d vinyl-scrobbler defaults`
---
+++ raspberrypi/daemon.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+### BEGIN INIT INFO
+# Provides:          vinyl-scrobbler
+# Required-Start:    autofs $all
+# Required-Stop:     $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Auto scrobbles audio
+# Description:       Listens to USB inputs and tries to identify songs using acoustid, then sends them to last.fm
+### END INIT INFO
+
+# PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+DAEMON="python /root/pi/checkouts/VinylScrobbler/master/raspberrypi/capture-audio.py"
+NAME=vinyl-scrobbler
+DESC="Vinyl Scrobbler"
+
+test -x $DAEMON || exit 0
+
+. /lib/lsb/init-functions
+
+set -e
+
+case "$1" in
+  start)
+    echo -n "Starting $DESC: "
+    start-stop-daemon --start --quiet --pidfile /var/run/$NAME.pid --background --make-pidfile --exec $DAEMON
+    echo "$NAME."
+    ;;
+  stop)
+    echo -n "Stopping $DESC: "
+    start-stop-daemon --stop --oknodo --quiet --pidfile /var/run/$NAME.pid
+    echo "$NAME."
+    ;;
+  restart)
+    $0 stop
+    sleep 1
+    $0 start
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart}" >&2
+    exit 1
+    ;;
+
+esac
+
+exit 0
