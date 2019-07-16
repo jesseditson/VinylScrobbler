7ae177d29a7f50c2e5ec83ba5b73088101810962
Jesse Ditson <jesse.ditson@gmail.com>
2019-07-15 21:34:26 -0700

    test out acrcloud for small fingerprints

--- raspberrypi/capture-audio.py
+++ raspberrypi/capture-audio.py
@@ -1,22 +1,22 @@
-import acoustid
 import pyaudio
+from acrcloud.recognizer import ACRCloudRecognizer, ACRCloudRecognizeType
+import wave
 
-apikey = "7C8vLuIYNq"
+# ACRCloud wants PCM 16 bit, mono 8000 Hz
+
 form_1 = pyaudio.paInt16  # 16-bit resolution
 chans = 1  # 1 channel
-samp_rate = 44100  # 44.1kHz sampling rate
+# samp_rate = 44100  # 44.1kHz sampling rate
+samp_rate = 8000  # 44.1kHz sampling rate
 chunk = 4096  # 2^12 samples for buffer
 record_secs = 10  # seconds to record
 
 audio = pyaudio.PyAudio()  # create pyaudio instantiation
 
-dev_index = None
+dev_index = 0
 for ii in range(audio.get_device_count()):
     if "USB" in audio.get_device_info_by_index(ii).get('name'):
         dev_index = ii
-
-if dev_index is None:
-    exit(1)
 
 print("capturing from audio device %s" % dev_index)
 
@@ -41,13 +41,25 @@ audio.terminate()
 
 # TODO: don't fingerprint if it's quiet
 
-# create a fingerprint from the audio file
-fingerprint = acoustid.fingerprint(samp_rate, chans, frames)
+wf = wave.open('tmp.wav', 'wb')
+wf.setnchannels(chans)
+wf.setsampwidth(audio.get_sample_size(form_1))
+wf.setframerate(samp_rate)
+wf.writeframes(b''.join(frames))
+wf.close()
 
-print("generated fingerprint: %s" % fingerprint)
+config = {
+    'host': 'identify-us-west-2.acrcloud.com',
+    'access_key': 'b7b74d5e2064676da61a8e563d5256c7',
+    'access_secret': 'hhOxrt5HQS20JRmBA6DsbbKSdfkT2miDv1Zo8j3Z',
+    'debug': True,
+    'timeout': 10,
+    'recognize_type': ACRCloudRecognizeType.ACR_OPT_REC_BOTH
+}
 
-matches = acoustid.lookup(apikey, fingerprint, record_secs)
+acrcloud = ACRCloudRecognizer(config)
+
+matches = acrcloud.recognize_by_file('tmp.wav', 0)
+# matches = acrcloud.recognize(stream)
 
 print("matches: %s" % matches)
-
-# for score, recording_id, title, artist in :
